fe823b411221cfaa2ae8b4f02555b34b297857bd
almin14 <88383501+almin14@users.noreply.github.com>
2021-12-15 12:46:34 +0530

    Add files via upload

--- page.php
+++ page.php
@@ -1,7 +1,4 @@
-<html>
-	<script type="text/javascript" src="jquery-3.6.0.js"></script>
-	<script type="text/javascript" src="js.js"></script>
-	<p></p>
-</html>
 <?php
+	include ('test.html');
+	echo "<p> World!</p>";
 ?>
