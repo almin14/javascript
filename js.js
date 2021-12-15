2cacc84f315dfb9393f38fe97ab1b84c5de22d59
almin14 <88383501+almin14@users.noreply.github.com>
2021-12-15 14:46:10 +0530

    Add files via upload

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	setInterval(function(){
+	var st = setInterval(function(){
 	$.ajax({
 		url:"page.php",
 		type:"POST",
@@ -11,4 +11,24 @@ $(document).ready(function(){
 		}
 	});
 	},2000);
+	$("#bt1").on("click",function(){
+		clearInterval(st);
+	});
+	$("#bt2").on("click",function(){
+		var st1 = setInterval(function(){
+		$.ajax({
+		url:"page.php",
+		type:"POST",
+		success:function(res){
+			console.log(res);
+			$("p").text("Hello world!");
+			console.log("hii");
+		}
+	});
+
+	},2000);
+		$("#bt1").on("click",function(){
+		clearInterval(st1);
+	});
+	});
 });
